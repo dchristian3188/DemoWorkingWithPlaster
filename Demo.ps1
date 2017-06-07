a2f610c55bf19b96125ffbe5dc1b430f887e7d48
dchristia3188 <dchristian3188@gmail.com>
2017-06-06 20:28:31 -0700

    updates

--- Demo.ps1
+++ Demo.ps1
@@ -45,3 +45,32 @@ ise "C:\Temp\FirstDemo\PlasterManifest.xml"
 Clear-Host
 Invoke-Plaster -TemplatePath C:\temp\FirstDemo\ -DestinationPath C:\temp\Go
 
+
+# Parameters
+
+## Reading input
+
+<parameter name="ModuleName" type="text" prompt="Name of your module" />
+<parameter name="ModuleDesc" type="text" prompt="Brief description on this module" />
+
+
+# Default values
+<parameter name="ModuleVersion" type="text" prompt="Version number (0.0.0.1)"  default='0.0.0.1' />
+
+
+## choice - Validateset
+
+<parameter name="Pester" type="choice" prompt="Include Pester Tests?" default='0'>
+    <choice label="&amp;Yes" help="Adds a pester folder" value="Yes" />
+    <choice label="&amp;No" help="Does not add a pester folder" value="No" />
+</parameter>
+
+## Multie Choice
+<parameter name="FunctionFolders" type="multichoice" prompt="Please select folders to include" default='0,1,2'>
+    <choice label="&amp;Public" help="Adds a public folder to module root" value="Public" />
+    <choice label="&amp;Internal" help="Adds a internal folder to module root" value="Internal" />
+    <choice label="&amp;Classes" help="Adds a classes folder to module root" value="Classes" />
+    <choice label="&amp;Binaries" help="Adds a binaries folder to module root" value="Binaries" />
+    <choice label="&amp;Data" help="Adds a data folder to module root" value="Data" />
+</parameter>
+
