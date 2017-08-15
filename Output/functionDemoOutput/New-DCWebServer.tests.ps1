cf9049161f1169c0b728df8cdf89771893abab75
dchristian3188 <dchristian3188@gmail.com>
2017-08-14 18:49:59 -0700

    Added material

---
+++ Output/functionDemoOutput/New-DCWebServer.tests.ps1
@@ -0,0 +1,5 @@
+Describe New-DCWebServer {
+    It "Needs to have real tests" {
+        $true | Should be $true
+    }
+}
---
+++ Output/functionDemoOutput/Remove-DCWebServer.tests.ps1
@@ -0,0 +1,5 @@
+Describe Remove-DCWebServer {
+    It "Needs to have real tests" {
+        $true | Should be $true
+    }
+}
