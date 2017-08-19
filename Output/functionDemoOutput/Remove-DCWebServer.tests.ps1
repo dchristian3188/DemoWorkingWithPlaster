42d18d484b314ec17d3bf088152f1cbc8acd76cd
dchristian3188 <dchristian3188@gmail.com>
2017-08-19 15:23:29 -0700

    Removed Output

--- Output/functionDemoOutput/Remove-DCWebServer.tests.ps1
+++
@@ -1,5 +0,0 @@
-Describe Remove-DCWebServer {
-    It "Needs to have real tests" {
-        $true | Should be $true
-    }
-}
--- Output/functionDemoOutput/Set-DCWebServer.tests.ps1
+++
@@ -1,5 +0,0 @@
-Describe Set-DCWebServer {
-    It "Needs to have real tests" {
-        $true | Should be $true
-    }
-}
